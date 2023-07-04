a07d54164bf0705b0e8b339140df34fcedee214a
seungchan baek <poketsc@gmail.com>
2023-07-04 19:27:35 +0900

    @Cacheable 이용한 캐싱 기능 구현

--- src/main/java/com/example/sessionstore/SessionStoreApplication.java
+++ src/main/java/com/example/sessionstore/SessionStoreApplication.java
@@ -2,8 +2,10 @@ package com.example.sessionstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SessionStoreApplication {
 
     public static void main(String[] args) {
